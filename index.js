643b5683df5867f2276d370f5041d2a89f9f7bca
chandankrishnan <chandan.mandre10@gmail.com>
2016-08-05 12:49:56 +0530

    add sensor true

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express'),
     routes = require('./routes/index'),
     bodyParser = require('body-parser'),
     path = require('path'),
-    port=process.env.PORT||8087,
+    port=process.env.PORT||8088,
     app = express();
 
 /** 
