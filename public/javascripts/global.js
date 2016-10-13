a0d26367ab6fc4817611679a05563552ab7fb4e6
chandankrishnan <chandan.mandre10@gmail.com>
2016-10-13 12:10:58 +0530

    Update global.js

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -137,7 +137,7 @@ function placesRequest(latlng, map) {
              */
             var infowindow = new google.maps.InfoWindow({})
             google.maps.event.addListener(thisplace, 'click', function() {
-                infowindow.setContent({{place.title}}+"<br/>" + place.vicinity);
+                infowindow.setContent(place.title+"<br/>" + place.vicinity);
                 infowindow.open(map, this);
             });
         })
